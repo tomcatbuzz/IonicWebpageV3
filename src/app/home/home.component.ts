55b3d91452b7488050bc46812cd4e6283dd3f4c9
Tony <tomcatbuzz@yahoo.com>
2024-12-11 00:16:42 -0500

    wip home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
+import { NgForOf } from '@angular/common';
 // import { ActivatedRoute } from '@angular/router';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonGrid, IonCol, IonRow } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButtons, IonButton, IonMenuButton, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonIcon, IonLabel, IonList, IonItem } from '@ionic/angular/standalone';
 
 import { HeaderComponent } from '../header/header.component';
 // import * as THREE from 'three';
@@ -12,17 +13,35 @@ import { gsap } from 'gsap';
 // import model from '../../assets/facefull.glb'
 import { CanvasCaseComponent } from '../canvas-case/canvas-case.component';
 
+import { chevronDownOutline, sendOutline, rocketOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    imports: [IonRow, IonCol, IonGrid, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, HeaderComponent, FooterComponent, ShuffleComponent, CanvasCaseComponent],
+    imports: [NgForOf, IonItem, IonList, IonIcon, IonLabel, IonRow, IonCol, IonGrid, IonHeader, IonToolbar, IonButtons, IonButton, IonMenuButton, IonTitle, IonContent, HeaderComponent, FooterComponent, ShuffleComponent, CanvasCaseComponent],
     standalone: true
 })
 export class HomeComponent  implements OnInit, OnDestroy {  
   private timeline: GSAPTimeline | null = null;
 
+  projects = [
+    { title: 'Project 1', description: 'Description of project 1' },
+    { title: 'Project 2', description: 'Description of project 2' },
+    // Add more projects as needed
+  ];
+
   constructor() { 
+    addIcons({ chevronDownOutline, sendOutline, rocketOutline })
+  }
+
+  navigateToProjects() {
+    // Implement navigation logic
+  }
+
+  navigateToContact() {
+    // Implement navigation logic
   }
 
   ngOnInit() {

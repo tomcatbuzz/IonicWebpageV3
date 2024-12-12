d787dcec91a9521d4506ab0aee6f840cdd769e3c
Tony <tomcatbuzz@yahoo.com>
2024-12-12 01:37:57 -0500

    wip on dark mode, worked home page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonHeader, IonButtons, IonButton, IonMenuButton, IonIcon, IonLabel, IonToolbar } from "@ionic/angular/standalone";
+import { IonHeader, IonButtons, IonButton, IonMenuButton, IonIcon, IonLabel, IonToolbar, IonToggle } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+// import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
     standalone: true,
-    imports: [ RouterLink, RouterLinkActive, IonHeader, IonButtons, IonButton, IonMenuButton, IonIcon, IonLabel, IonToolbar],
+    imports: [FormsModule, IonToggle,  RouterLink, RouterLinkActive, IonHeader, IonButtons, IonButton, IonMenuButton, IonIcon, IonLabel, IonToolbar],
     
 })
 export class HeaderComponent {
+  isDarkMode = false;
 
   constructor() { 
     addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
-  }  
+  } 
+  
+  toggleDarkMode(event: CustomEvent) {
+    this.isDarkMode = event.detail.checked;
+    document.body.classList.toggle('dark', this.isDarkMode);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NgForOf } from '@angular/common';
 // import { ActivatedRoute } from '@angular/router';
-import { IonHeader, IonToolbar, IonButtons, IonButton, IonMenuButton, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonIcon, IonLabel, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonToolbar, IonButtons, IonButton, IonMenuButton, IonTitle, IonContent, IonGrid, IonCol, IonRow, IonIcon, IonLabel, IonList, IonItem, IonImg } from '@ionic/angular/standalone';
 
 import { HeaderComponent } from '../header/header.component';
 // import * as THREE from 'three';
@@ -20,28 +21,21 @@ import { addIcons } from 'ionicons';
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    imports: [NgForOf, IonItem, IonList, IonIcon, IonLabel, IonRow, IonCol, IonGrid, IonHeader, IonToolbar, IonButtons, IonButton, IonMenuButton, IonTitle, IonContent, HeaderComponent, FooterComponent, ShuffleComponent, CanvasCaseComponent],
+    imports: [RouterLink, NgForOf, IonImg, IonItem, IonList, IonIcon, IonLabel, IonRow, IonCol, IonGrid, IonToolbar, IonButtons, IonButton, IonMenuButton, IonTitle, IonContent, HeaderComponent, FooterComponent, ShuffleComponent, CanvasCaseComponent],
     standalone: true
 })
 export class HomeComponent  implements OnInit, OnDestroy {  
   private timeline: GSAPTimeline | null = null;
 
   projects = [
-    { title: 'Project 1', description: 'Description of project 1' },
-    { title: 'Project 2', description: 'Description of project 2' },
+    { title: 'The Sprinkler Guy', description: 'Business Website', url: 'thesprinklerguyjb.com', img: "" },
+    { title: 'Project 2', description: 'Description of project 2', url: 'some.com',
+      img: "" },
     // Add more projects as needed
   ];
 
   constructor() { 
     addIcons({ chevronDownOutline, sendOutline, rocketOutline })
-  }
-
-  navigateToProjects() {
-    // Implement navigation logic
-  }
-
-  navigateToContact() {
-    // Implement navigation logic
   }
 
   ngOnInit() {
@@ -64,7 +58,13 @@ export class HomeComponent  implements OnInit, OnDestroy {
         delay: 1
       } 
     });
-    this.timeline.fromTo('.canvasWrapper', { opacity: 0 },
+    // this.timeline.fromTo('.canvasWrapper', { opacity: 0 },
+    //   { opacity: 1, duration: 1.7, ease: 'sin.inOut' }
+    // );
+    this.timeline.fromTo('.hero-text', { opacity: 0 },
+      { opacity: 1, duration: 1.7, ease: 'sin.inOut' }
+    );
+    this.timeline.fromTo('.image-container', { opacity: 0 },
       { opacity: 1, duration: 1.7, ease: 'sin.inOut' }
     );
     console.log('nothing')
